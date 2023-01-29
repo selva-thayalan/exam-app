ad50a0e3b6bf8ce7ddc9c2829daf31c661132a2f
Thaya <selvathayalan.r@gmail.com>
2023-01-29 13:03:37 +0530

    Installed react-router. -Removed App component and added ApplicationRouter page.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ApplicationRoute from './Application';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ApplicationRoute />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
