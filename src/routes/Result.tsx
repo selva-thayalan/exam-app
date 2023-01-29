acaa6e384d9a1b7ccc6a95780b7e33266a80ed04
Thaya <selvathayalan.r@gmail.com>
2023-01-30 03:06:46 +0530

    Result page for test submission preview is implemented.

--- src/routes/Result.tsx
+++ src/routes/Result.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import ResultsContext from "../contexts/Results";
+import { ITestQuestion, ITestResult, getQuestion } from "../constants";
+import QuestionCard from "../components/QuestionCard";
 
 interface ResultRouteProps {
     
 }
  
-const ResultRoute: React.FunctionComponent<ResultRouteProps> = () => {
-    return ( null );
+const ResultRoute: React.FunctionComponent<ResultRouteProps> = (props) => {
+    const { resultid } = useParams();
+    const context = useContext(ResultsContext);
+
+    const tQuestion: ITestQuestion | undefined = getQuestion() || undefined;
+    const tResult: ITestResult | any = context.getResult((resultid as unknown) as number);
+
+    return ( 
+        <div className="result-preview">
+            {tQuestion.questions.map((que, i) => <QuestionCard key={que.questionid} model={que} answer={tResult.answers[i]}/>)}
+        </div>
+     );
 }
  
 export default ResultRoute;
